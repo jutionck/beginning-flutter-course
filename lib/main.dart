13b3ec427aeed5bd1d51c55a63e51f802f441a5f
Jution Candra Kirana <jutionck@gmail.com>
2022-02-27 20:03:22 +0700

    Pengenalan tentang AppBar Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+/// AppBar merupakan sebuah widget yang berada di top position UI
+/// AppBar terdiri dari : Leading, Title, Actions, Flexible Space dan Bottom
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -11,8 +14,22 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false, // menghilangkan banner showDebugBanner
       home: Scaffold(
         backgroundColor: Colors.amber[600],
-        appBar: AppBar(
-          title: Text('Widget Dasar'),
+        appBar: AppBar( // sebetulnya appBar ini membutuhkan PreferredSizeWidget, tetapi karena AppBar sudah extends jadi tidak perlu lagi
+          leading: Icon(Icons.code), // biasanya icon
+          leadingWidth: 50, // mengatur lebar leading
+          title: Text('APPBAR'),
+          titleSpacing: 10, // mengatur spaci judul ke leading
+          centerTitle: false, // default pada Android adalah false, ios true
+          actions: [
+            IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: () {},
+            )
+          ],
+          bottom: PreferredSize(
+            preferredSize: Size.fromHeight(200), child: Container(),
+          ),
+          flexibleSpace: Text('Flexible Text'),
         ),
         body: Center(
           child: Text('Hello'),
