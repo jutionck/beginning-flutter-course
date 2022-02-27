c690a145eff4c12d1e025296e0b5ab16753d5c37
Jution Candra Kirana <jutionck@gmail.com>
2022-02-28 01:44:33 +0700

    Penggunaan faker untuk ListTile Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,67 @@
+import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
 
-/// Image Widget adalah sebuah widget untuk menampilkan sebuah image atau gambar
-/// Image yang didukung juga banyak seperti jpg, jpeg, png, svg, webp, dll
-/// Jenis Provider Image bisa menggunakan assets (pada project kita buat folder image)
-/// Network pemanggilan image melalui url secara random, contoh pada web https://picsum.photos/200/300
-/// File -> sumber image berasal dari luar project atau di Device
-/// Memory -> sumber image berasal dari memory
-/// Kecepatan Provider : memory - assets - file - network
-/// Jangan lupa untuk mendaftarkan file image pada pubspec.yaml
+/// Extract Widget sebenenrnya kita membuat sebuah class baru kemudian class tersebut bisa kita panggil berulang-ulang
+/// Contoh disino kita membuat sebuah widget dari ListTile
+/// Kita akan membuat data dummy, install dev pada pubspec.yaml : faker: ^1.3.0
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final List<Color> myColor = [
-    Colors.red,
-    Colors.amber,
-    Colors.lightBlue,
-    Colors.green
-  ];
-
+  var faker = new Faker();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false, // menghilangkan banner showDebugBanner
       home: Scaffold(
-        appBar: AppBar(
-          // sebetulnya appBar ini membutuhkan PreferredSizeWidget, tetapi karena AppBar sudah extends jadi tidak perlu lagi
-          leading: Icon(Icons.code), // biasanya icon
-          title: Text('IMAGE WIDGET'),
-          centerTitle: true, // default pada Android adalah false, ios true
-          actions: [
-            IconButton(
-              icon: Icon(Icons.refresh),
-              onPressed: () {},
-            )
-          ],
-        ),
-        body: Center(
-          child: Container(
-            width: 350,
-            height: 500,
-            color: Colors.amber,
-            // child: Image(
-            //   fit: BoxFit.cover, // terdiri dari fill, filHeight, fillWidth, cover dll
-            //   image: AssetImage("images/image_1.jpeg"),
-            //   // image: NetworkImage("https://picsum.photos/200/300"),
-            // ),
-            child: Image.asset(
-              "images/image_2.jpeg",
-              fit: BoxFit.cover,
-            ),
+          appBar: AppBar(
+            // sebetulnya appBar ini membutuhkan PreferredSizeWidget, tetapi karena AppBar sudah extends jadi tidak perlu lagi
+            leading: Icon(Icons.code), // biasanya icon
+            title: Text('Chat Sample'),
+            centerTitle: true, // default pada Android adalah false, ios true
+            actions: [
+              IconButton(
+                icon: Icon(Icons.refresh),
+                onPressed: () {},
+              )
+            ],
           ),
-        ),
+          body: ListView.builder(
+            itemCount: 100,
+            itemBuilder: (context, index) {
+              return ChatItem(
+                imageUrl: "https://picsum.photos/id/$index/200/300",
+                title: faker.person.name(),
+                subTitle: faker.lorem.sentence(),
+                trailing: faker.date.time(),
+              );
+            },
+          )),
+    );
+  }
+}
+
+class ChatItem extends StatelessWidget {
+  final String imageUrl;
+  final String title;
+  final String subTitle;
+  final String trailing;
+
+  const ChatItem(
+      {required this.imageUrl,
+      required this.title,
+      required this.subTitle,
+      required this.trailing});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(imageUrl),
       ),
+      title: Text(title),
+      subtitle: Text(subTitle),
+      trailing: Text(trailing),
     );
   }
 }
