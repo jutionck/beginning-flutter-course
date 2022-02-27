4af44c53df438b64e60dc86b9fc99160d731cda4
Jution Candra Kirana <jutionck@gmail.com>
2022-02-28 03:11:25 +0700

    Praktik Flexible Layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,60 @@
 import 'package:flutter/material.dart';
 
-/// Stateful widget, adanya perubahan widget
-/// Perubahan itu di tandai dengan fungsi setState
+/// Flexible widget sangat penting dalam menerapkan tampilan
+/// Karena di Android setiap tipe handphone pasti mempunyai layar yang berbeda-beda
+/// Kecuali ios sudah jelas untuk layar
 
 void main() => runApp(MyApp());
 
-// convert dari stateless -> statefull (wind: alt + enter. mac: option + enter)
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  int counter = 1;
-  List<Widget> widgets = [];
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner:
-            false, // menghilangkan banner showDebugBanner
-        home: Scaffold(
-          appBar: AppBar(
-            leading: Icon(Icons.code), // biasanya icon
-            title: Text('Counter Sample'),
-            centerTitle: true, // default pada Android adalah false, ios true
-            actions: [
-              IconButton(
-                icon: Icon(Icons.refresh),
-                onPressed: () {},
-              )
-            ],
-          ),
-          body: ListView(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          widgets.add(
-                            Text("Data ke " + counter.toString(), style: TextStyle(fontSize: 30),),
-                          );
-                          counter++;
-                        });
-                      },
-                      child: Text("Tambah Data")),
-                  ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          widgets.removeLast();
-                          counter--;
-                        });
-                      },
-                      child: Text("Hapus Data"))
-                ],
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: widgets,
-              )
-            ],
-          ),
-        ));
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flexible Layout"),
+        ),
+        body: Column(
+          children: [
+            Flexible(child: Row(
+              children: [
+                Flexible(
+                    flex: 1,
+                    child: Container(
+                      margin: EdgeInsets.all(5),
+                      color: Colors.green,
+                    )),
+                Flexible(
+                    flex: 1,
+                    child: Container(
+                      margin: EdgeInsets.all(5),
+                      color: Colors.amber,
+                    )),
+                Flexible(
+                    flex: 1,
+                    child: Container(
+                      margin: EdgeInsets.all(5),
+                      color: Colors.purple,
+                    )),
+              ],
+            )),
+            Flexible(
+                flex: 2,
+                child: Container(
+                  margin: EdgeInsets.all(5),
+                  color: Colors.red,
+                )),
+            Flexible(
+                flex: 1,
+                child: Container(
+                  margin: EdgeInsets.all(5),
+                  color: Colors.lightBlue,
+                )),
+          ],
+        ),
+      ),
+    );
   }
 }
