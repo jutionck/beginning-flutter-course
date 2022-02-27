3d488004b0eb2bffaf5216d04ebddd99a341b80a
Jution Candra Kirana <jutionck@gmail.com>
2022-02-27 20:42:47 +0700

    Pengenalan Column, Row dan Stack Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 
-/// TextWidget merupakan sebuah widget untuk mengatur tulisan atau text
-/// TextWidget terdiri dari banyak properti : maxLine, overflow, textStyle dll
+/// Colum Widget digunakan utnuk menyusun widget-widget ke bawah atau secara vertikal
+/// Row Widget digunakan untuk menyusun widget-widget ke samping atau horizontal
+/// Stack Widget digunakan untuk menyusun widget-widget secara tumpuk/stack
+/// Column dan Row mempunyai properti mainAxis dan Cross Axis
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  // List<Color> listColor = [Colors.red, Colors.amber, Colors.lightBlue];
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +19,7 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar( // sebetulnya appBar ini membutuhkan PreferredSizeWidget, tetapi karena AppBar sudah extends jadi tidak perlu lagi
           leading: Icon(Icons.code), // biasanya icon
-          title: Text('TEXT WIDGET'),
+          title: Text('COLUMN ROW STACK'),
           centerTitle: true, // default pada Android adalah false, ios true
           actions: [
             IconButton(
@@ -24,24 +28,70 @@ class MyApp extends StatelessWidget {
             )
           ],
         ),
-        body: Center(
-          child: Text(
-              'Hello Hola Halo Hai Hai Hello ',
-            // maxLines: 2, // mengatur maksimal beris pada text
-            textAlign: TextAlign.right,
-            overflow: TextOverflow.ellipsis, // membuat text di akhir menjadi ...
-            style: TextStyle(
-              backgroundColor: Colors.amber, // mengatur background font
-              color: Colors.black, // mengatur warna font
-              fontSize: 40, // mengatur ukuran font
-              fontWeight: FontWeight.normal, // mengatur ketebalan font
-              letterSpacing: 2, // mengatur jarak antar huruf
-              fontFamily: 'Fredoka', // ini bisa cari di https://fonts.google.com/ -> update pada pubspec.yaml
-              decoration: TextDecoration.underline,
-              // decorationStyle: TextDecorationStyle.dotted,
-              decorationColor: Colors.red
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: 50,
+              height: 50,
+              color: Colors.red,
             ),
-          ),
+            Container(
+              width: 50,
+              height: 50,
+              color: Colors.amber,
+            ),
+            Container(
+              width: 50,
+              height: 50,
+              color: Colors.lightBlue,
+            ),
+            Container(
+              width: 50,
+              height: 50,
+              color: Colors.green,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.deepOrange,
+                ),
+                Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.pink,
+                )
+              ],
+            ),
+            Stack(
+              children: [
+                Container(
+                  width: 300,
+                  height: 300,
+                  color: Colors.black12,
+                ),
+                Container(
+                  width: 200,
+                  height: 200,
+                  color: Colors.orangeAccent,
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.lightBlue,
+                ),
+                Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.deepPurple,
+                )
+              ],
+            )
+          ],
         ),
       ),
     );
