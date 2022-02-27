0a8aa93381734bddd385c90a80b68b63c307bf21
Jution Candra Kirana <jutionck@gmail.com>
2022-02-28 06:21:58 +0700

    Latihan Membuat Form

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,20 @@
 import 'package:flutter/material.dart';
-
-/// Latihan Stack dan Align Widget
+import 'package:flutter_course/screens/app_form.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-class _MyAppState extends State<MyApp> {
-  final TextEditingController myController = TextEditingController();
-  String result = "Hasil Input";
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Text("Enigma Apps"),
         ),
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                TextField(
-                  decoration: InputDecoration(
-                      // memberikan decoration icon diluar field
-                      icon: Icon(Icons.person),
-                      border: OutlineInputBorder(),
-                      hintText: "Masukkan nama anda",
-                      labelText: "Full Name" // memberikan label field
-                      ),
-                  controller: myController,
-                  onChanged: (value) {
-                    print("onchange");
-                  },
-                  onSubmitted: (value) {
-                    print(value);
-                    setState(() {
-                      result = value;
-                    });
-                  },
-                  onEditingComplete: () {
-                    print("Sukses Edit");
-                  },
-                ),
-                Text(result)
-              ],
-            ),
-          ),
-        ),
+        body: AppForm(),
       ),
     );
   }
