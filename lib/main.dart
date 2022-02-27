ea6edf9e2735911b6ca8031e90e76d71df658896
Jution Candra Kirana <jutionck@gmail.com>
2022-02-27 21:15:43 +0700

    Pengenalan ListTile Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 
-/// ListView Widget digunakan untuk menyusun widget-widget yang tidak diketahui batasannya
-/// ListView dapat di scroll
-/// Scroll dapat di atur tergantung mau ke arah mana vertikal maupun horizontal
-/// ListView.builder mempunyai sebuah properti itemCount dan itemBuilder
-/// ListView.separated mempunya properti tambahan separatedBuilder berfungsi untuk memberikan batasan antar widget
+/// ListTile Widget adalah sebuah widget list yang didalamnya terdapat sebuah widget lainnya
+/// Widget lainnya dapat berupa leading, text dan lain lain
+/// Contoh penggunaan adalah ketika kita membuat sebuah list berita yang terdiri dari image, judul, dan sub judul
+/// Contoh lain adalah misalnya list chat pesan seperti wa, line dll
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final List<Color> myColor = [Colors.red, Colors.amber, Colors.lightBlue, Colors.green];
+  final List<Color> myColor = [
+    Colors.red,
+    Colors.amber,
+    Colors.lightBlue,
+    Colors.green
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +23,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           // sebetulnya appBar ini membutuhkan PreferredSizeWidget, tetapi karena AppBar sudah extends jadi tidak perlu lagi
           leading: Icon(Icons.code), // biasanya icon
-          title: Text('LIST VIEW'),
+          title: Text('LIST TILE'),
           centerTitle: true, // default pada Android adalah false, ios true
           actions: [
             IconButton(
@@ -28,20 +32,46 @@ class MyApp extends StatelessWidget {
             )
           ],
         ),
-        body: ListView.separated(
-          separatorBuilder: (context, index) {
-            return Divider(
+        body: ListView(
+          children: [
+            ListTile(
+              title: Text('Jution Candra Kirana'),
+              subtitle: Text('Halo apa kabar teman'),
+              leading: CircleAvatar(),
+              trailing: Text('10:10 PM'),
+            ),
+            Divider(
               color: Colors.black26,
-            );
-          },
-          itemCount: myColor.length,
-          itemBuilder: (contex, index) {
-            return Container(
-              width: 300,
-              height: 300,
-              color: myColor[index],
-            );
-          },
+            ),
+            ListTile(
+              title: Text('Destry Faradila Nur\'avisa'),
+              subtitle: Text(
+                'Bagaiamna keadaan kamu saat ini apakah baik-baik saja ?',
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+              ),
+              leading: CircleAvatar(),
+              trailing: Text('10:10 PM'),
+            ),
+            Divider(
+              color: Colors.black26,
+            ),
+            ListTile(
+              title: Text('Jution Candra Kirana'),
+              subtitle: Text('Halo apa kabar teman'),
+              leading: CircleAvatar(),
+              trailing: Text('10:10 PM'),
+            ),
+            Divider(
+              color: Colors.black26,
+            ),
+            ListTile(
+              title: Text('Jution Candra Kirana'),
+              subtitle: Text('Halo apa kabar teman'),
+              leading: CircleAvatar(),
+              trailing: Text('10:10 PM'),
+            )
+          ],
         ),
       ),
     );
